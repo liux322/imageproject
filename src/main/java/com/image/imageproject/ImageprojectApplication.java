b4b8bcf95e762500e059be00271bc141b2c1c02d
dliu <dliu@realmatters.com>
2018-05-03 13:18:17 -0400

    new project

---
+++ src/main/java/com/image/imageproject/ImageprojectApplication.java
@@ -0,0 +1,12 @@
+package com.image.imageproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageprojectApplication.class, args);
+    }
+}
